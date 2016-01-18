8d1cb35496d3184721b5b598294357cc72a0ac6b
smndtrl <smndtrl@openmailbox.org>
2016-01-18 17:51:57 +0100

    Add missing Log.cs

---
+++ Signal/util/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signal.Util
+{
+    class Log
+    {
+        public static void Warn(string msg)
+        {
+            System.Diagnostics.Debug.WriteLine($"Warn: {msg}");
+        }
+
+        public static void Debug(string msg)
+        {
+            System.Diagnostics.Debug.WriteLine($"Debug: {msg}");
+        }
+    }
+
+    
+}
