e27016e1132e9faef8517d117d59257de2e9feca
AndHell <fallentheorie@gmail.com>
2016-02-29 20:37:35 +0100

    renaming and clean up
    thread => chat
    directory => contact

--- Signal/Views/MainPage.xaml.cs
+++ Signal/Views/MainPage.xaml.cs
@@ -23,17 +23,17 @@ namespace Signal.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ThreadMasterPage : SignalPage
+    public sealed partial class MainPage : SignalPage
     {
-        public ThreadViewModel ViewModel
+        public MainPageViewModel ViewModel
         {
             get
             {
-                return (ThreadViewModel)DataContext;
+                return (MainPageViewModel)DataContext;
             }
         }
 
-        public ThreadMasterPage()
+        public MainPage()
         {
             this.InitializeComponent();
 
@@ -56,6 +56,11 @@ namespace Signal.Views
                 RefreshBtn.Visibility = Visibility.Visible;
                 SelectItmesBtn.Visibility = Visibility.Collapsed;
             }
+        }
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
